7c696b8bc3fdecf48ebddd8b479d4c02949f08f8
ckh <751722804@qq.com>
2021-05-19 14:00:08 +0800

    n

---
+++ MNIST_Dataset/try_load_data.py
@@ -0,0 +1,5 @@
+from load_data import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True)
+print("x_train's type: ", type(x_train))
+print("x_train's shape: ", x_train.shape)
